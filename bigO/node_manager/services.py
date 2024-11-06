3db84794b4daa6edd2b25eeabf4d6ea672cc8fe6
Amir Khalife <eng.amir.bu@gmail.com>
2024-11-06 23:56:28 +0330

    getting easytier dest route

--- bigO/node_manager/services.py
+++ bigO/node_manager/services.py
@@ -1,11 +1,14 @@
+import ipaddress
 import logging
 
-from .models import Node
+from django.db.models import Subquery
+
+from . import models
 
 logger = logging.getLogger(__name__)
 
 
-def node_spec_create(*, node: Node, ip_a: str):
+def node_spec_create(*, node: models.Node, ip_a: str):
     """
     makes decisions based on the node current state(spec)
     """
@@ -25,3 +28,17 @@ def node_spec_create(*, node: Node, ip_a: str):
             else:
                 container_spec.ipv6 = res
         container_spec.save()
+
+def get_easytier_to_node_ips(*, source_node: models.Node, dest_node_id: int) -> list[ipaddress.IPv4Address]:
+    source_ea_networks = models.EasyTierNetwork.objects.filter(network_easytiernodes__node_id=source_node.id)
+    dest_ea_nodes = models.EasyTierNode.objects.filter(node_id=dest_node_id, network_id__in=Subquery(source_ea_networks.values("id")))
+    dest_node = models.Node.objects.get(id=dest_node_id)
+    res = []
+    for i in dest_ea_nodes:
+        if i.ipv4:
+            res.append(i.ipv4)
+        elif i.node.container_spec and i.node.container_spec.ipv4:
+            res.append(i.node.container_spec.ipv4)
+        else:
+            logger.warning(f"no easytier destination from {source_node=} to {dest_node=}")
+    return res
