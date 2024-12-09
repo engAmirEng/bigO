93d8429a35f7da44da671499ce89e8fdb0736ca1
Amir Khalife <eng.amir.bu@gmail.com>
2024-12-10 02:41:54 +0330

    check_node_latest_sync_task fixes

--- bigO/node_manager/tasks.py
+++ bigO/node_manager/tasks.py
@@ -25,30 +25,30 @@ def check_node_latest_sync(*, limit_seconds: int, ignore_node_ids: list[int] | N
     now = timezone.now()
     limit_seconds = timedelta(seconds=limit_seconds)
     ignore_node_ids = ignore_node_ids or []
-    all_nodelatestsyncstat_qs = models.NodeLatestSyncStat.objects.filter(respond_at__lt=now - limit_seconds)
-    nodelatestsyncstat_qs = all_nodelatestsyncstat_qs.exclude(node_id__in=ignore_node_ids)
+    all_offlines_qs = models.NodeLatestSyncStat.objects.filter(respond_at__lt=now - limit_seconds)
+    reporting_offlines_qs = all_offlines_qs.exclude(node_id__in=ignore_node_ids)
     perv_offline_nodes = cache.get("offline_nodes")
-    back_onlines = models.NodeLatestSyncStat.objects.none()
+    back_onlines_qs = models.NodeLatestSyncStat.objects.none()
     if perv_offline_nodes:
         perv_offline_node_ids = json.loads(perv_offline_nodes)
-        back_onlines = models.NodeLatestSyncStat.objects.filter(id__in=perv_offline_node_ids).exclude(
-            id__in=Subquery(all_nodelatestsyncstat_qs.values("id"))
+        back_onlines_qs = models.NodeLatestSyncStat.objects.filter(node_id__in=perv_offline_node_ids).exclude(
+            id__in=Subquery(all_offlines_qs.values("id"))
         )
 
-    if nodelatestsyncstat_qs.count() == 0 and not back_onlines.exists():
+    if reporting_offlines_qs.count() == 0 and not back_onlines_qs.exists():
         return "all_good"
 
     message = render_to_string(
         "node_manager/annonces/node_latest_sync.thtml",
         context={
             "limit_timedelta": limit_seconds,
-            "nodelatestsyncstat_qs": nodelatestsyncstat_qs,
-            "back_online_qs": back_onlines,
+            "reporting_offlines_qs": reporting_offlines_qs,
+            "back_online_qs": back_onlines_qs,
         },
     )
     session = AiohttpSession()
     bot = aiogram.Bot(settings.TELEGRAM_BOT_TOKEN, session=session, parse_mode=ParseMode.HTML)
     async_to_sync(session.close)()
     async_to_sync(bot.send_message)(chat_id=superuser.telegram_chat_tid, text=message)
-    cache.set("offline_nodes", json.dumps([i.node_id for i in nodelatestsyncstat_qs]))
-    return f"{str(nodelatestsyncstat_qs.count())} are down and {str(back_onlines.count())} are back"
+    cache.set("offline_nodes", json.dumps([i.node_id for i in all_offlines_qs]))
+    return f"{str(reporting_offlines_qs.count())} are down and {str(back_onlines_qs.count())} are back"
