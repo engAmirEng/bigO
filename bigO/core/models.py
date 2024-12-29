a9cecc2075ad18aaa921aeebab5c463c6f38ecf5
Amir Khalife <eng.amir.bu@gmail.com>
2024-12-30 03:16:01 +0330

    rethink asymetric

--- bigO/core/models.py
+++ bigO/core/models.py
@@ -4,28 +4,31 @@ from bigO.utils.models import TimeStampedModel
 from django.db import models
 
 
-class CryptographicObject(PolymorphicModel, TimeStampedModel, models.Model):
+class AbstractCryptographicObject(models.Model):
     class AlgorithmChoices(models.IntegerChoices):
         RSA = 1, "RSA"
         ECDSA = 2, "ECDSA"
 
     slug = models.SlugField(unique=True)
     algorithm = models.PositiveSmallIntegerField(AlgorithmChoices.choices)
-    fingerprint = models.CharField(max_length=64, blank=False, db_index=True)
     content = models.TextField()
+
+    class Meta:
+        abstract = True
 
     def __str__(self):
         return f"{self.pk}-{self.slug}"
 
 
-class PrivateKey(CryptographicObject):
+class PrivateKey(AbstractCryptographicObject, TimeStampedModel, models.Model):
     passphrase = models.CharField(max_length=255, null=True, blank=True)
     key_length = models.PositiveSmallIntegerField()
 
 
-class PublicKey(CryptographicObject):
+class Certificate(PolymorphicModel, AbstractCryptographicObject, TimeStampedModel, models.Model):
+    fingerprint = models.CharField(max_length=64, blank=False, db_index=True)
     private_key = models.ForeignKey(PrivateKey, on_delete=models.CASCADE, null=True, blank=True)
-    parent_public_key = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
+    ca_certificate = models.ForeignKey("self", on_delete=models.CASCADE, null=True, blank=True)
     valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
     subject = models.CharField(max_length=255)
