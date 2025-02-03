21b5cde61202f7e7c3e6672c492f434dd6d446f7
Amir Khalife <eng.amir.bu@gmail.com>
2025-02-03 11:08:25 +0330

    brought back NodeLatestSyncStatInline

--- bigO/node_manager/admin.py
+++ bigO/node_manager/admin.py
@@ -42,7 +42,7 @@ class NodeSupervisorConfigInline(admin.StackedInline):
 
 @admin.register(models.Node)
 class NodeModelAdmin(admin_extra_buttons.mixins.ExtraButtonsMixin, admin.ModelAdmin):
-    inlines = [NodePublicIPInline, NodeSupervisorConfigInline, NodeInnerProgramInline, ]
+    inlines = [NodePublicIPInline, NodeSupervisorConfigInline, NodeInnerProgramInline, NodeLatestSyncStatInline]
     list_display = (
         "__str__",
         "agent_spec_display",
