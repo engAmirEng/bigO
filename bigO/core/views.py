ba8063530cc4e561d3fc50e2bb3f4d028d853f9f
Amir Khalife <eng.amir.bu@gmail.com>
2025-03-25 23:31:28 +0330

    [tmp_rz1]

--- bigO/core/views.py
+++ bigO/core/views.py
@@ -10,3 +10,16 @@ async def nginx_flower_auth_request(request):
     if not request.user.is_superuser:
         return HttpResponse(status=403)
     return HttpResponse(status=200)
+
+async def tmp_rz1(request):
+    import django.shortcuts
+    import random
+    from functools import partial
+    from django.utils import timezone
+    from zoneinfo import ZoneInfo
+    return django.shortcuts.render(
+        request, "core/tmp_rz1.html", {
+            "random_int": partial(random.randint, 1, 100),
+            "last_update_at": timezone.localtime(timezone=ZoneInfo("Asia/Tehran"))
+        }
+    )
