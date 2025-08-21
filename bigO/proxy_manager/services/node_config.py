7fcb74c8a75dc4160e7d68b15c678bbce81086b6
Amir Khalife <eng.amir.bu@gmail.com>
2025-08-21 21:21:55 +0330

    rm redundant code form merge

--- bigO/proxy_manager/services/node_config.py
+++ bigO/proxy_manager/services/node_config.py
@@ -855,52 +855,6 @@ def get_strategy_part(
     return strategy_part, sorted_balancer_members[0]["tag"]
 
 
-def get_agent_current_subscriptionperiods_qs(agent: models.Agent):
-    return models.SubscriptionPeriod.objects.filter(profile__initial_agency_id=agent.agency_id)
-
-
-def set_profile_last_stat(
-    sub_profile_id: str, sub_profile_period_id: str, collect_time: datetime.datetime
-) -> models.SubscriptionPeriod | None:
-    subscriptionperiod = models.SubscriptionPeriod.objects.filter(
-        id=sub_profile_period_id, profile_id=sub_profile_id
-    ).first()
-    if subscriptionperiod is None:
-        logger.critical(f"no SubscriptionPeriod found with {sub_profile_id=} and {sub_profile_period_id=}")
-        return None
-    if subscriptionperiod.first_usage_at is None:
-        subscriptionperiod.first_usage_at = collect_time
-    if subscriptionperiod.first_usage_at > collect_time:
-        subscriptionperiod.first_usage_at = collect_time
-
-    if subscriptionperiod.last_usage_at is None:
-        subscriptionperiod.last_usage_at = collect_time
-    if subscriptionperiod.last_usage_at < collect_time:
-        subscriptionperiod.last_usage_at = collect_time
-    subscriptionperiod.save()
-    return subscriptionperiod
-
-
-def set_internal_user_last_stat(
-    rule_id: str, node_user_id: str, collect_time: datetime.datetime
-) -> models.InternalUser | None:
-    internaluser = models.InternalUser.objects.filter(connection_rule_id=rule_id, node_id=node_user_id).first()
-    if internaluser is None:
-        logger.critical(f"no InternalUser found with {rule_id=} and {node_user_id=}")
-        return
-    if internaluser.first_usage_at is None:
-        internaluser.first_usage_at = collect_time
-    if internaluser.first_usage_at > collect_time:
-        internaluser.first_usage_at = collect_time
-
-    if internaluser.last_usage_at is None:
-        internaluser.last_usage_at = collect_time
-    if internaluser.last_usage_at < collect_time:
-        internaluser.last_usage_at = collect_time
-    internaluser.save()
-    return internaluser
-
-
 class OutboundProtocol(Protocol):
     name: str
     to_inbound_type: models.InboundType
