f243f4d8764b001f209a5bde4d8d14fcfd643e75
Amir Khalife <eng.amir.bu@gmail.com>
2025-10-17 17:09:42 +0330

    basic AppRouter for modular aiogram usage

--- bigO/telegram_bot/dispatchers.py
+++ bigO/telegram_bot/dispatchers.py
@@ -1,10 +1,25 @@
 import os
+from typing import Any, Awaitable, Callable
 
 from redis.asyncio import Redis
 
 from aiogram import Dispatcher
+from aiogram import Router as BaseRouter
+from aiogram.dispatcher.event.bases import UNHANDLED
 from aiogram.fsm.storage.redis import DefaultKeyBuilder, RedisStorage
 
 fsm_storage = RedisStorage(Redis.from_url(os.getenv("REDIS_URL")), key_builder=DefaultKeyBuilder(with_bot_id=True))
 
 dp = Dispatcher(storage=fsm_storage)
+
+
+class AppRouter(BaseRouter):
+    def __init__(self, *args, app_filter_callback: Callable[[Any], Awaitable[bool]], **kwargs):
+        super().__init__(*args, **kwargs)
+        self.app_filter_callback = app_filter_callback
+
+    async def propagate_event(self, *args, **kwargs):
+        handle = await self.app_filter_callback(*args, **kwargs)
+        if not handle:
+            return UNHANDLED
+        return await super().propagate_event(*args, **kwargs)
