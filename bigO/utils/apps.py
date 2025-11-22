a0eef0f5469d6b3a5fe0827f4463988d0331ed09
Amir Khalife <eng.amir.bu@gmail.com>
2025-11-22 11:01:47 +0330

    [utils] fix render_value_in_context for digits_locale

--- bigO/utils/apps.py
+++ bigO/utils/apps.py
@@ -17,21 +17,23 @@ def new_render_value_in_context(value, context):
     monkey patch the original django's render_value_in_context to add:
     1-digits_locale
     """
-    # 1-digits_locale
     import django.utils.formats
     from django.utils.formats import localize
     from django.utils.html import conditional_escape
     from django.utils.timezone import template_localtime
 
-    if isinstance(value, str):
+    value = template_localtime(value, use_tz=context.use_tz)
+    value = localize(value, use_l10n=context.use_l10n)
+
+    # 1-digits_locale
+    if not issubclass(type(value), str):
+        value = str(value)
         if getattr(context, "active_digits_locale", False):
             locale_digits_fn = django.utils.formats.get_format("LOCALE_DIGITS")
             if callable(locale_digits_fn):
                 value = locale_digits_fn(text=value)
     # end 1
 
-    value = template_localtime(value, use_tz=context.use_tz)
-    value = localize(value, use_l10n=context.use_l10n)
     if context.autoescape:
         if not issubclass(type(value), str):
             value = str(value)
